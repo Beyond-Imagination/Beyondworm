fa0b89528fd0a5623f670ad549ed502b6b362e4d
gangjung <cru6548@gmail.com>
2025-06-20 00:03:51 +0900

    index.ts가 진입점이 되도록 수정

--- web-io-game/src/index.ts
+++ web-io-game/src/index.ts
@@ -1,9 +1,11 @@
 // filepath: /web-io-game/web-io-game/src/index.ts
-import { Server } from './server/server';
-import { GameEngine } from './game/gameEngine';
+import './client/client.ts';
 
-const server = new Server();
-const gameEngine = new GameEngine();
+// import { Server } from './server/server';
+// import { GameEngine } from './game/gameEngine';
 
-server.start();
-gameEngine.start();
+// const server = new Server();
+// const gameEngine = new GameEngine();
+
+// server.start();
+// gameEngine.start();
