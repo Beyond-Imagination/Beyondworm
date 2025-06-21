4fc2039801441d272dd62ffbd724dc1e1bf5a251
gangjung <cru6548@gmail.com>
2025-06-21 19:12:36 +0900

    vite.config의 base값 설정

--- web-io-game/vite.config.ts
+++ web-io-game/vite.config.ts
@@ -1,7 +1,8 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     root: ".",           // index.html 이 root 에 있으니 그대로
+    base: '/Beyondworm/',
     build: {
         outDir: "dist/client",
         emptyOutDir: true,
