26598ed2294fc2879e91ff5a1113789ce3e241dd
gangjung <cru6548@gmail.com>
2025-06-21 18:26:34 +0900

    Vite build를 위한 세팅값 추가

--- web-io-game/vite.config.ts
+++ web-io-game/vite.config.ts
@@ -1,10 +1,17 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     root: ".",           // index.html 이 root 에 있으니 그대로
     build: {
         outDir: "dist/client",
         emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['phaser'] // 'phaser'를 vendor.js로 분리
+                }
+            }
+        }
     },
     resolve: {
         alias: {
