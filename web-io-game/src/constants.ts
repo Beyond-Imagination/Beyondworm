348f47f8398af8ee11308e9c015fd73ac2fd8aa9
konempty <jameswebb@naver.com>
2025-08-27 20:51:18 +0900

    BEYONDWORM-89 카메라가 드드득 거리는 원인 해결

--- web-io-game/src/constants.ts
+++ web-io-game/src/constants.ts
@@ -1,6 +1,7 @@
 export const FE_CONSTANTS = {
     // Camera
-    CAMERA_LERP_SPEED: 0.08, // 카메라 줌 보간 속도 (0~1, 클수록 빠르게 따라감)
+    CAMERA_MOVE_LERP_SPEED: 1, // 카메라 움직임 보간 속도 (0~1, 클수록 빠르게 따라감)
+    CAMERA_ZOOM_LERP_SPEED: 0.08, // 카메라 줌 보간 속도
 
     // UI
     ZORDER_SEGMENT: 1000,
