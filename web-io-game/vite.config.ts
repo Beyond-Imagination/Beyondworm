34ac170e25b2ec963ad762b1621ec39a62779e2c
gangjung <cru6548@gmail.com>
2025-11-30 23:36:12 +0900

    shared 프로젝트를 프론드(vite)와 서버(commonjs)에서 각각 사용할 수 있도록 모듈을 나눠 빌드하도록 수정.

--- web-io-game/vite.config.ts
+++ web-io-game/vite.config.ts
@@ -3,11 +3,13 @@ import path from "path";
 
 // defineConfig는 함수를 인자로 받을 수 있으며, 이 함수는 현재 명령어('serve' 또는 'build')를 기반으로 설정을 반환합니다.
 export default defineConfig(({ command }) => {
-    // 명령어에 따라 shared 라이브러리의 올바른 경로를 결정합니다.
-    const sharedPath =
-        command === "serve"
-            ? path.resolve(__dirname, "../shared/src") // 개발 서버의 경우, 원본 소스 파일을 사용합니다.
-            : path.resolve(__dirname, "../shared/dist"); // 프로덕션 빌드의 경우, 빌드된 파일을 사용합니다.
+    const alias = {
+        "@": path.resolve(__dirname, "src"), // 편리한 경로 단축
+    };
+
+    if (command === "serve") {
+        alias["@beyondworm/shared"] = path.resolve(__dirname, "../shared/src"); // 개발 서버의 경우, 원본 소스 파일을 사용합니다.
+    }
 
     return {
         root: ".", // index.html 이 root 에 있으니 그대로
@@ -16,11 +18,12 @@ export default defineConfig(({ command }) => {
             outDir: "dist/client",
             emptyOutDir: true,
         },
+        optimizeDeps: {
+            include: ["@beyondworm/shared"],
+        },
         resolve: {
-            alias: {
-                "@": "/src", // 편리한 경로 단축
-                "@beyondworm/shared": sharedPath, // 동적으로 결정된 경로를 사용합니다.
-            },
+            alias, // 동적으로 결정된 경로를 사용합니다.
         },
     };
 });
+
